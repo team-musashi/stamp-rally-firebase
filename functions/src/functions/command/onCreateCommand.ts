e823b63238537ffb4f1182ab51538896eb603bb6
nozomi-koborinai <kobofender@gmail.com>
2023-02-21 23:28:01 +0900

    🚀 feat: 経路算出コマンド発生に対するハンドラを作成 -> まずはDirections APIを叩いてレスポンスをjsonとして受け取るところまで
    Closes #30

--- functions/src/functions/command/onCreateCommand.ts
+++ functions/src/functions/command/onCreateCommand.ts
@@ -4,16 +4,21 @@ import { commandConverter } from './../../firestore-collection/command/commandCo
 import { constants } from '../../config/constants'
 import 'reflect-metadata'
 import * as functions from 'firebase-functions'
+import * as googleMaps from '@google/maps'
 import { PublicStampRallyRepository } from '../../firestore-collection/public-stamp-rally/publicStampRallyRepository'
 import { container, providers } from '../../config/dicon'
 import { UserRepository } from '../../firestore-collection/user/userRepository'
 import { Command } from '../../firestore-collection/command/entity/command'
+import { LatLng } from '@google/maps'
+import { TravelMode, TravelRestriction } from '@googlemaps/google-maps-services-js'
+import { GeoPoint } from 'firebase-admin/firestore'
 
 /**
  * コマンドが作成された
  */
 export const onCreateCommand = functions
   .region(constants.region)
+  .runWith({ secrets: [`GOOGLE_MAP_API_KEY`] })
   .firestore.document(`/command/{id}`)
   .onCreate(async (snapshot) => {
     const command = commandConverter.fromFirestore(snapshot)
@@ -40,6 +45,10 @@ export const onCreateCommand = functions
 
       case `withdrawStampRally`:
         await withdrawStampRally(command)
+        break
+
+      case `calculateRoute`:
+        await calculateRoute(command)
         break
 
       default:
@@ -140,4 +149,77 @@ const withdrawStampRally = async (command: Command) => {
     entryStampId: stampRallyId,
   })
   functions.logger.info(`参加中スタンプラリーを中断ステータスに変換しました`)
+}
+
+/**
+ * スタンプラリースポットの経路を算出する
+ */
+const calculateRoute = async (command: Command) => {
+  // ユーザーIDがなければ処理終了
+  if (!command.uid) {
+    return
+  }
+
+  // コマンドからスタンプラリーIDが取得できなければ処理終了
+  const stampRallyId = command.data?.get(`stampRallyId`) as string
+  if (!stampRallyId) {
+    return
+  }
+
+  // SecretManagerからGoogle Map APIのAPIキーが取得できなければ処理終了
+  const apiKey = process.env.GOOGLE_MAP_API
+  if (apiKey === undefined) {
+    return
+  }
+
+  // 公開スタンプラリーが取得できなければ処理終了
+  const publicStampRallyRepository = container.get<PublicStampRallyRepository>(providers.publicStampRallyRepository)
+  const publicStampRally = await publicStampRallyRepository.get({ id: stampRallyId })
+  if (!publicStampRally) {
+    return
+  }
+
+  // 公開スタンプラリー中の公開スポットが取得できなければ処理終了
+  const publicSpots = await publicStampRallyRepository.getSpots({ id: stampRallyId })
+  if (publicSpots.length == 0) {
+    return
+  }
+
+  // Google Map APIサービスの初期化
+  const mapClient = googleMaps.createClient({ key: apiKey })
+
+  // 該当スタンプラリー中の全スポット間の経路を求める
+  // let stampRallyRoute: GeoPoint[]
+  for (let i = 0; i < publicSpots.length - 1; i++) {
+    if (publicSpots[i].location == null || publicSpots[i + 1].location == null) {
+      continue
+    }
+    const from: LatLng = {
+      latitude: (publicSpots[i].location! as any as GeoPoint).latitude,
+      longitude: (publicSpots[i].location! as any as GeoPoint).longitude,
+    }
+    const to: LatLng = {
+      latitude: (publicSpots[i + 1].location! as any as GeoPoint).latitude,
+      longitude: (publicSpots[i + 1].location! as any as GeoPoint).longitude,
+    }
+
+    // 経路算出（Directions）API呼び出し
+    mapClient.directions(
+      {
+        origin: from,
+        destination: to,
+        mode: TravelMode.walking,
+        avoid: [TravelRestriction.highways, TravelRestriction.tolls, TravelRestriction.ferries],
+      },
+      (error, response) => {
+        // コールバック処理にてAPIレスポンスを受け取る
+        if (error) {
+          functions.logger.error(error)
+        } else {
+          // TODO：一旦jsonの中身をfunctionsのログに出力している
+          functions.logger.info(response.json.routes)
+        }
+      }
+    )
+  }
 }
